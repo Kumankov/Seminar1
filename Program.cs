f5c668329d87462134a82134c61c27d789477919
Ilya <kumankov@gmail.ru>
2022-11-21 23:11:27 +0300

    ДЗ

--- Program.cs
+++ Program.cs
@@ -1,3 +1,37 @@
-﻿int N = new Random().Next(100,999);
-Console.WriteLine(N);
-Console.Write($"{N/100}{N%10}");
+﻿Task10 ();
+void Task10 ()
+{
+Console.WriteLine("Введите трехзначное число для задачи 10:");
+int N = int.Parse(Console.ReadLine());
+if (Math.Abs(N)>999 || Math.Abs(N)<100)
+{
+    Console.WriteLine($"Число {N} не трехзначное");
+}
+else 
+{
+    Console.WriteLine($"Вторая цифра {(N%100-N%10)/10}");
+}
+}
+
+Task13();
+void Task13 ()
+{
+Console.WriteLine("Введите число для задачи 13:");
+string? N13 = Console.ReadLine();
+int lenght = N13.Length;
+if (lenght<3)
+Console.WriteLine("Третьей цифры нет");
+else
+Console.WriteLine(N13.Substring(2,1));
+}
+
+Task15();
+void Task15 ()
+{
+Console.WriteLine("Введите цифру для задачи 15:");
+int day = int.Parse(Console.ReadLine());
+if ((day==6)||(day==7))
+Console.WriteLine("Выходной");
+else
+Console.Write("Не выходной");
+}
